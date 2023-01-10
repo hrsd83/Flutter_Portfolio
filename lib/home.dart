57a82321aa5f42d57d61a3f416dcedf0b18d4b38
Henry Sanchez <hrsd83@gmail.com>
2023-01-10 14:37:39 -0300

    Segundo commit de portfolio

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sliding_sheet/sliding_sheet.dart';
+import 'package:animate_do/animate_do.dart';
 
 class MyHome extends StatefulWidget {
   const MyHome({super.key});
@@ -10,18 +11,52 @@ class MyHome extends StatefulWidget {
 }
 
 class _MyHomeState extends State<MyHome> {
-  myArchives(num, type) {
+  myArchives(type) {
     return Row(
       children: [
-        Text(
-          num,
-          style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
         Container(
           margin: const EdgeInsets.only(top: 10),
-          child: Text(type),
+          child: Text(
+            type,
+            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.w900),
+          ),
         )
       ],
+    );
+  }
+
+  mySpecialized(icon, tecnol) {
+    return Container(
+      margin: EdgeInsets.all(0.8),
+      width: 105,
+      height: 115,
+      child: BounceInUp(
+        child: Card(
+          margin: const EdgeInsets.all(5),
+          color: Colors.black87,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(icon),
+                const SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 25),
+                  child: Text(
+                    tecnol,
+                    style: const TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 
@@ -31,12 +66,14 @@ class _MyHomeState extends State<MyHome> {
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
     return Scaffold(
+      //fondo de Background
       backgroundColor: Colors.black,
       extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.transparent,
       ),
+      //Hoja deslizante
       body: SlidingSheet(
         elevation: 8,
         cornerRadius: 50,
@@ -45,26 +82,106 @@ class _MyHomeState extends State<MyHome> {
           snappings: [0.4, 0.7, 1.0],
           positioning: SnapPositioning.relativeToAvailableSpace,
         ),
-        body: const Center(
-          child: Text(
-            'This widget is below the SlidingSheet',
-            style: TextStyle(color: Colors.white70),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.75,
+                  child: Image.asset('assets/fondoPantalla.png')),
+              Container(
+                alignment: Alignment.center,
+                margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.49),
+                child: Column(
+                  children: const [
+                    Text(
+                      'Henry Sánchez',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      'Software Developer',
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
         builder: (context, state) {
           return Container(
               height: 500,
               margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
-              child: Column(children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    myArchives('78', 'Projects'),
-                    myArchives('78', 'Projects'),
-                    myArchives('78', 'Projects'),
-                  ],
-                )
-              ]));
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        myArchives('Flutter Developer'),
+                        // myArchives('50', 'Clients'),
+                        // myArchives('86', 'Messages'),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    const Text(
+                      'Skills',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    //Especializaciones
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            mySpecialized(null, 'HTML'),
+                            mySpecialized(null, 'CSS'),
+                            mySpecialized(null, 'JavasScript'),
+                          ],
+                        ),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            mySpecialized(
+                              null,
+                              'Flutter',
+                            ),
+                            mySpecialized(
+                              null,
+                              'Git &\nGitHub',
+                            ),
+                            mySpecialized(null, 'VisualStudio'),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            mySpecialized(
+                              null,
+                              'FireBase',
+                            ),
+                            mySpecialized(null, 'Postman'),
+                          ],
+                        )
+                      ],
+                    )
+                  ]));
         },
       ),
     );
